959d24689cae2c0d573b05b2f8fd01a926e57aaa
MrValdez <s_cloudx@yahoo.com>
2011-12-03 15:28:51 +0800

    explicit checking against RGB; moving of code blocks into functions; added code to run tweetdeck if its not running; added timeout

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import win32api, win32con
+import win32com.client
+import subprocess
 import PIL
 import Image
 import ImageGrab
+import os
 
 # 1. Take a screenshot of the desktop.
 # 2. If TweetDeck isn't running, run it. (possibly put this first)
@@ -13,8 +17,12 @@ import ImageGrab
 # 9. ???
 # 10. Profit!
 
-screenshot = ImageGrab.grab()
-TweetDeck_TextBox = Image.open("click_here.png")
+# helpful link:
+# http://code.activestate.com/recipes/65107/
+
+# bugs:
+# * will fail if you have another program with the name TweetDeck running
+#	(such as a windows explorer folder)
 
 def CompareImage(Haystack, Needle):
 	"""
@@ -33,7 +41,11 @@ def CompareImage(Haystack, Needle):
 	for y in range(HaystackBBox[3] - NeedleBBox[3]):
 		for x in range(HaystackBBox[2] - NeedleBBox[2]):
 			location = (y * HaystackBBox[2]) + x
-			if Haystack_RawData[location] == Needle_RawData[0]:
+
+			# we check against each other's RGB
+			if Haystack_RawData[location][0] == Needle_RawData[0][0] and \
+			   Haystack_RawData[location][1] == Needle_RawData[0][1] and \
+			   Haystack_RawData[location][2] == Needle_RawData[0][2]:
 				# possible first pixel found
 				# create crop
 
@@ -43,11 +55,14 @@ def CompareImage(Haystack, Needle):
 				Found = True
 				i = 0
 				for pixel in CroppedHaystack_RawData:
-					if Needle_RawData[i] != pixel:
+					if Needle_RawData[i][0] != pixel[0] and \
+					   Needle_RawData[i][1] != pixel[1] and \
+					   Needle_RawData[i][2] != pixel[2]:
 						Found = False
 						break
 
 					i = i + 1
+
 				if i == 0:
 					continue
 
@@ -56,7 +71,6 @@ def CompareImage(Haystack, Needle):
 				CroppedHaystack.load()
 				CroppedHaystack.save("debug2.png")
 
-				#if i == len(CroppedHaystack_RawData) - 1:
 				if Found:
 					bbox = [x, y, x + NeedleBBox[2], y + NeedleBBox[3]]
 					CroppedHaystack = Haystack.crop(bbox)
@@ -74,26 +88,85 @@ def CompareImage(Haystack, Needle):
 
 	return None, None
 
-import win32com.client
-shell = win32com.client.Dispatch("WScript.Shell")
-shell.AppActivate('TweetDeck')
+def ClickOnTweetdeck(x, y):
+	offset = 10
+	x = x + offset
+	y = y + offset
 
-x, y = CompareImage(screenshot, TweetDeck_TextBox)
+	win32api.SetCursorPos((x,y))
+	win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+	win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
 
-if x == None or y == None:
-	print ("Not found")
+def SendMessage(message):
+	shell.SendKeys(message)
+	shell.SendKeys("~")
 
-import win32api, win32con
-def click(x,y):
-    win32api.SetCursorPos((x,y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+def RunTweetDeck():
+	# check if TweetDeck is already running. If not, try to run it.
+	def FindExe(ExePath):
+				#http://stackoverflow.com/questions/1187264/how-to-check-if-some-process-is-running-in-task-manager-with-python
+		import win32com.client
+		strComputer = "."
+		objWMIService = win32com.client.Dispatch("WbemScripting.SWbemLocator")
+		objSWbemServices = objWMIService.ConnectServer(strComputer,"root\cimv2")
+		colItems = objSWbemServices.ExecQuery("Select * from Win32_Process")
+		for objItem in colItems:
+		   if objItem.ExecutablePath == ExePath:
+			   return True
+		return False
 
-print x,y
-offset = 10
-click(x + offset, y + offset)
+	#win32ui.FindWindow(None, "TweetDeck")
 
-# http://code.activestate.com/recipes/65107/
+	# check x86 first
+	environKey = "ProgramFiles(x86)"
+	exe = os.environ[environKey] + r"\TweetDeck\TweetDeck.exe"
 
-#shell.SendKeys("It works! Just a few more cleanup... https://github.com/MrValdez/TweetDeck-interface")
-#shell.SendKeys("~")
+	if not os.path.isfile(exe):
+		environKey = "ProgramFiles"
+		exe = os.environ[environKey] + r"\TweetDeck\TweetDeck.exe"
+
+	if not os.path.isfile(exe):
+		print ("TweetDeck not found in Program Files")
+		return False
+
+	#subprocess.call([exe])
+	subprocess.Popen([exe])
+	return True
+
+def Main():
+	shell = win32com.client.Dispatch("WScript.Shell")
+
+	TweetDeck_TextBox = Image.open("click_here.png")
+
+	if not RunTweetDeck():
+		print ("Tweetdeck was not found")
+		return
+
+	# keep looking for the tweetdeck's textbox until 1min has passed
+	import time
+	timer = time.clock()
+	timeout = timer + 60.0
+
+	while True:
+		shell.AppActivate('TweetDeck')
+		screenshot = ImageGrab.grab()
+
+		x, y = CompareImage(screenshot, TweetDeck_TextBox)
+		if not (x == None and y == None):
+			break
+
+		print ("Looking for Tweetdeck... (%.0f seconds left)" % (timeout - timer))
+
+		time.sleep(1)
+		timer = time.clock()
+		if (timer > timeout):
+			print ("Timeout: Tweetdeck was not found")
+			return
+
+	if x == None or y == None:
+		print ("Tweetdeck was not found")
+	else:
+		ClickOnTweetdeck(x, y)
+		#SendMessage("")
+
+Main()
